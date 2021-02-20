112f43d9ac7ab27cd028b29077e3b9d948dac8dc
Antonio <antoniofiorentino16@gmail.com>
2021-02-20 19:25:46 +0100

    inizio Menu categories

--- js/script.js
+++ js/script.js
@@ -1,7 +1,41 @@
 var app = new Vue({
  el: '#root',
  data:{
+   menuCategories:[
+     {
+       name: 'APPETIZERS',
+       image: `$bg-appetizers`,
+     },
+     {
+       name: 'BURGERS',
+       image: `$bg-burgers`,
+     },
+     {
+       name: 'PIZZAS',
+       image: `$bg-pizzas`,
+     },
+     {
+       name: 'FRIES',
+       image: `$bg-fries`,
+     },
+     {
+       name: 'SIDES',
+       image: `$bg-sides`,
+     },
+     {
+       name: 'DESSERTS',
+       image: `$bg-desserts`,
+     },
+     {
+       name: 'BEVERAGES',
+       image: `$bg-beverages`,
+     },
+     {
+       name: 'SPECIALS',
+       image: `$bg-specials`,
+     },
 
+   ]
  }
 });
 Vue.config.devtools = true
