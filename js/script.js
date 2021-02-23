1726a9a19d9af2342eb4b2a2840286adc546d194
Antonio <antoniofiorentino16@gmail.com>
2021-02-23 17:55:41 +0100

    Esercizio

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 var app = new Vue({
  el: '#root',
  data:{
+   active: false,
+   deactivate: true,
+   active2: false,
+   deactivate2: true,
+   active3: false,
+   deactivate3: true,
    menuCategories:[
      {
        name: 'APPETIZERS',
@@ -35,7 +41,23 @@ var app = new Vue({
        image: 'img/bg-specials.jpg',
      },
 
-   ]
+   ],
+ },
+
+ methods:{
+   mouseOverFries: function(){
+     this.active = !this.active;
+     this.deactivate = !this.deactivate;
+   },
+   mouseOverFrappe: function(){
+     this.active2 = !this.active2;
+     this.deactivate2 = !this.deactivate2;
+   },
+   mouseOverDonut: function(){
+     this.active3 = !this.active3;
+     this.deactivate3 = !this.deactivate3;
+   }
+
  }
 });
 Vue.config.devtools = true
